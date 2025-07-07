524c74d575e4a7baf48895cff64660874181b839
Antwi-tech <nanaafuaantwiwaa624@gmail.com>
2025-07-07 23:36:36 +0000

    calculating the simple interest over a period of time

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,9 @@
+#  Calculating for simple interest over a given period of time (I = P * R * T)
+
+principal  = 1000   # in dollars
+rate = 0.05 # in percentage
+time = 3 # in years
+
+interest = principal * rate * time
+
+print(f"The simple interest is: {interest}")
