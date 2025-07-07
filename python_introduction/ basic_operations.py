717f9d6671c8a0825305715ebf5df3335c03bc35
Antwi-tech <nanaafuaantwiwaa624@gmail.com>
2025-07-07 16:37:57 +0000

    into to python

---
+++ python_introduction/ basic_operations.py
@@ -0,0 +1,18 @@
+# numbers for arithmether operations
+number1 = 10
+number2 = 5
+
+# sum of both first and second numbers
+sum = number1 + number2
+print("Addition of", number1, "and", number2,"is", sum)
+
+# subraction of fisrt and secont number
+subraction = number1 - number2
+print("Subraction of", number1, "and", number2,"is", subraction)
+
+# multiply first number by second number
+multiplication = number1 * number2
+print("Multiplication of", number1, "and", number2,"is", multiplication)
+
+
+
