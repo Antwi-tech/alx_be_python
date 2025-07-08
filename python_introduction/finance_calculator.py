89afb2a685ab19f09bbabeb60d7b3ccb8314da0f
Antwi-tech <nanaafuaantwiwaa624@gmail.com>
2025-07-08 15:11:59 +0000

    corrected variable names to the specified variable names given

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,14 +1,14 @@
 # Projectinf user saving based on expenditure
 
-mth_income =  int(input("Enter your monthly income: ") )
-mth_expenses =  int(input("Enter your total monthly expenses: "))
+monthly_income =  int(input("Enter your monthly income: ") )
+monthly_expenses =  int(input("Enter your total monthly expenses: "))
 
 # Monthy savings according to user expenses and income
-mth_savings = mth_income - mth_expenses  
+monthly_savings = monthly_income - monthly_expenses 
 
-print(f"Your monthly savings are ${mth_savings:.2f}.")
+print(f"Your monthly savings are ${monthly_savings:.2f}.")
 
 # Annual savings of user
-projected_savings = mth_savings * 12 + (mth_savings * 12 * 0.05)
+projected_savings = monthly_savings * 12 + (monthly_savings * 12 * 0.05)
 
 print(f"Projected savings after one year, with interest, is: ${projected_savings:.2f}.")
