c9eda6f02c2cf5c45e8afc7aec5cad5c74026d13
Antwi-tech <nanaafuaantwiwaa624@gmail.com>
2025-07-08 14:45:03 +0000

    corrected prompt and changed prompt variable name

--- python_introduction/future_age_calculator.py
+++ python_introduction/future_age_calculator.py
@@ -1,7 +1,7 @@
 #  Calculating a user future age according to present age
 
-usr_age= input("Hpw old are you? ")
+prompt= input("How old are you? ")
 
-age = int (usr_age) + 27
+age = int (prompt) + 27
 
 print(f"In 2050, you will be {age} years old")
