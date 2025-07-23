20a331e9573c651fc7b495bb941619413dde40b7
Antwi-tech <nanaafuaantwiwaa624@gmail.com>
2025-07-23 20:14:29 +0000

    rm operation

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,4 +1,4 @@
-def perform_operation(num1, num2 , operation ):
+def perform_operation(num1, num2, operation):
     """
     Performs basic arithmetic operations based on the provided operation string.
 
