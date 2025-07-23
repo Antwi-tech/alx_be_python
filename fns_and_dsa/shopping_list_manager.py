eab150f8fa980b3a4f2362e7bc2105720d1d5a81
Antwi-tech <nanaafuaantwiwaa624@gmail.com>
2025-07-23 20:55:18 +0000

    corrected version

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -1,34 +1,40 @@
 def display_menu():
-    print("\nShopping List Manager")
+    print("Shopping List Manager")
     print("1. Add Item")
     print("2. Remove Item")
     print("3. View List")
     print("4. Exit")
 
 def main():
-    shopping_list = []
+    shopping_list = []  # Array to store shopping items
+
     while True:
         display_menu()
-        choice = input("\nEnter your choice: ")
+        try:
+            choice = int(input("Enter your choice: "))
+        except ValueError:
+            print("Invalid input. Please enter a number.")
+            continue
 
-        if choice == '1':
-            item = input("\nEnter item to add: ")
+        if choice == 1:
+            item = input("Enter item to add: ")
             shopping_list.append(item)
-            pass
-        elif choice == '2':
-            item = input("\nEnter item to remove: ")
+            print(f"'{item}' added to shopping list.")
+        elif choice == 2:
+            item = input("Enter item to remove: ")
             if item not in shopping_list:
                 print("Item does not exist")
-            else: 
-                shopping_list.remove(item)   
-                print(f"Item has been removed")
-            pass
-        elif choice == '3':
-            print("\nYour shoping list:\n ")
-            for item in shopping_list:
-                print(item)
-            pass
-        elif choice == '4':
+            else:
+                shopping_list.remove(item)
+                print("Item has been removed")
+        elif choice == 3:
+            print("\nYour shopping list:")
+            if shopping_list:
+                for item in shopping_list:
+                    print(f"- {item}")
+            else:
+                print("Shopping list is empty.")
+        elif choice == 4:
             print("Goodbye!")
             break
         else:
