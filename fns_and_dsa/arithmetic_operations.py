83692a2c69f47601a02df3f9a62c8df38eabccdd
Antwi-tech <nanaafuaantwiwaa624@gmail.com>
2025-07-23 20:10:02 +0000

    Aritmetic as a modules

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,15 +1,24 @@
+def perform_operation(num1, num2, operation):
+    """
+    Performs basic arithmetic operations based on the provided operation string.
 
-def perform_operation(num1 : float, num2 : float ,operation : str):
+    Parameters:
+        num1: First number
+        num2: Second number
+        operation: One of 'add', 'subtract', 'multiply', or 'divide'
+
+    Returns:
+        The result of the operation, or an error message string
+    """
     if operation == 'add':
-       return num1 + num2
+        return num1 + num2
     elif operation == 'subtract':
         return num1 - num2
     elif operation == 'multiply':
         return num1 * num2
-    elif operation == "divide":
+    elif operation == 'divide':
+        if num2 == 0:
+            return "Division by zero error"
         return num1 / num2
     else:
         return "Invalid operation"
-      
-    
-    
